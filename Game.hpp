19d3b659a51594d39749c03b3f91ec165055b5d4
j-tesla <58279279+j-tesla@users.noreply.github.com>
2020-01-18 09:50:10 +0530

    Update Game.hpp
    dynamic attacker, defender pair

--- Game.hpp
+++ Game.hpp
@@ -20,8 +20,25 @@ namespace MyStrategy
   // You can also make new functions and call them from game function.
   void game(BeliefState *state)
   {
-	  attacker(state,2);
-	  defender(state,1);
-	  goalkeeper(state,0);
+      int a, d, g = 0;
+
+      if (state->homePos[1].x > state->homePos[2].x)
+      {
+          a = 1;
+          d = 2;
+      }
+      else
+      {
+          a = 2;
+          d = 1;
+      }
+
+	  attacker(state,a);
+	  
+      if (state->ballPos.x < -HALF_FIELD_MAXX/2) attacker(state, d);
+      else defender(state, d);
+
+      /*if (state->pr_ball_in_our_dbox) attacker(state, 0);
+      else*/ goalkeeper(state,g);
   }
 }
