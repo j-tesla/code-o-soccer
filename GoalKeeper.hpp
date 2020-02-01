014a011617f9a6fc0f0cdc6650b3d7d292388bef
j-tesla <58279279+j-tesla@users.noreply.github.com>
2020-02-01 07:53:13 +0530

    Update GoalKeeper.hpp

--- GoalKeeper.hpp
+++ GoalKeeper.hpp
@@ -22,17 +22,42 @@ namespace MyStrategy
 	{
 		//print("GoalKeeper\n");
 
-		Vec2D dpoint(OUR_GOAL_X + DBOX_HEIGHT, (int)rayCastY(state, botID));
+		Vec2D dpoint;
 
+		dpoint.x = OUR_GOAL_X + DBOX_HEIGHT;
+		if (state->ballVel.x < 0)
+		{
+			dpoint.y = (int)rayCastY(state, botID);
+		}
+		else
+		{
+			dpoint.y = state->ballPos.y;
+		}
 
 		if (dpoint.y > OUR_GOAL_MAXY)
 			dpoint.y = OUR_GOAL_MAXY;
 		if (dpoint.y < OUR_GOAL_MINY)
 			dpoint.y = OUR_GOAL_MINY;
-		
-		if (state->ballVel.x < 0)
+		/*if (state->ballPos.x < -HALF_FIELD_MAXX + 300)
 		{
-			GoToPoint(botID, state, dpoint, PI / 2, false,false);
+			dpoint y 
 		}
+		*/
+
+
+		GoToPoint(botID, state, dpoint, PI / 2, false, false);
+
+		if (state->pr_ball_in_our_dbox)
+		{
+			if (state->ballPos.y < state->homePos[botID].y)
+				Spin(botID, -MAX_BOT_OMEGA);
+			if (state->ballPos.y > state->homePos[botID].y)
+				Spin(botID, MAX_BOT_OMEGA);
+
+		}
+		
+		
+
+		
 	}
 }
